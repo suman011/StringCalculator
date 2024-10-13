a8ea07a6c5af6f4b65bd233abe2157bda4e463aa
suman <sumannimbalkar272@gmail.com>
2024-10-13 16:00:31 +0530

    Add support for custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -2,6 +2,11 @@ def add(numbers):
     if numbers == "":
         return 0
     else:
-        numbers = numbers.replace("\n", ",")
-        nums = map(int, numbers.split(","))
+        delimiter = ","
+        if numbers.startswith("//"):
+            parts = numbers.split("\n", 1)
+            delimiter = parts[0][2:]
+            numbers = parts[1]
+        numbers = numbers.replace("\n", delimiter)
+        nums = map(int, numbers.split(delimiter))
         return sum(nums)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -12,5 +12,7 @@ def test_single_number(self):
         self.assertEqual(add("1,2,3,4,5"), 15)
         def test_newline_between_numbers(self):
         self.assertEqual(add("1\n2,3"), 6)
+        def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
 if __name__ == '__main__':
     unittest.main()
