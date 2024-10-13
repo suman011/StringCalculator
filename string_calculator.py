e88c5a8c53937923c1f24f0989fb3c577912c1d5
suman <sumannimbalkar272@gmail.com>
2024-10-13 15:57:31 +0530

    Add single number support

--- string_calculator.py
+++ string_calculator.py
@@ -1,3 +1,5 @@
 def add(numbers):
     if numbers == "":
         return 0
+    elif numbers.isnumeric():
+        return int(numbers)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,6 +4,7 @@ from string_calculator import add
 class TestStringCalculator(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(add(""), 0)
-
+def test_single_number(self):
+        self.assertEqual(add("1"), 1)
 if __name__ == '__main__':
     unittest.main()
