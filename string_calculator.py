984e90bc3cb6cb09cd7f89d4573146b4a76a164f
suman <sumannimbalkar272@gmail.com>
2024-10-13 15:59:40 +0530

    Add support for newlines between numbers

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,6 @@ def add(numbers):
     if numbers == "":
         return 0
     else:
+        numbers = numbers.replace("\n", ",")
         nums = map(int, numbers.split(","))
         return sum(nums)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -10,5 +10,7 @@ def test_single_number(self):
         self.assertEqual(add("1,5"), 6)
         def test_multiple_numbers(self):
         self.assertEqual(add("1,2,3,4,5"), 15)
+        def test_newline_between_numbers(self):
+        self.assertEqual(add("1\n2,3"), 6)
 if __name__ == '__main__':
     unittest.main()
