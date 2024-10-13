97ed5a26858f7c8b33cc462209e870ebfddf95f1
suman <sumannimbalkar272@gmail.com>
2024-10-13 15:58:10 +0530

    Support two numbers separated by commas

--- string_calculator.py
+++ string_calculator.py
@@ -3,3 +3,6 @@ def add(numbers):
         return 0
     elif numbers.isnumeric():
         return int(numbers)
+    else:
+        nums = map(int, numbers.split(","))
+        return sum(nums)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -6,5 +6,7 @@ class TestStringCalculator(unittest.TestCase):
         self.assertEqual(add(""), 0)
 def test_single_number(self):
         self.assertEqual(add("1"), 1)
+        def test_two_numbers(self):
+        self.assertEqual(add("1,5"), 6)
 if __name__ == '__main__':
     unittest.main()
